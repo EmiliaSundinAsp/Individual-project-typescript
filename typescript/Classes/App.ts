07ce7144a10794fb74f2a476c10f929bac5b85f9
EmiliaSundinAsp <emiliasundinasp@gmail.com>
2024-10-13 11:26:40 +0200

    create loop to play the game

--- typescript/Classes/App.ts
+++ typescript/Classes/App.ts
@@ -2,4 +2,23 @@ import prompt from "../helpers/prompt.js";
 import Board from './Board.js';
 import Player from './Player.js';
 
-export default class App {}
+export default class App {
+
+  board: Board;
+  playerX: Player;
+  playerO: Player;
+
+
+  constructor() {
+    // Loop to play the game
+    while (true) {
+      this.createPlayers();
+      console.log('Player created')
+      this.board = new Board;
+      this.startGameLoop();
+      this.whoHasWonOnGameOver();
+      let playAgain = prompt('Play again? (yes/no) ')
+      if (playAgain !== 'yes') { break; }
+    }
+  }
+}
