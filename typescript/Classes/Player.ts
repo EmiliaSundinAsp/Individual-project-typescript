99746b9903a0da8bdc3dbddaa506472978268e31
EmiliaSundinAsp <emiliasundinasp@gmail.com>
2024-10-13 19:52:51 +0200

    Add player class

--- typescript/Classes/Player.ts
+++ typescript/Classes/Player.ts
@@ -3,5 +3,14 @@ import Board from "./Board.js";
 export default class Player {
 
   name: string;
+  color: string;
+  board: Board;
+  isComputer: boolean;
 
+  constructor(name: string, color: string, board: Board, isComputer: boolean = false) {
+    this.name = name;
+    this.color = color;
+    this.board = board;
+    this.isComputer = isComputer;
+  }
 }
